0905af0a852f0b9bc2b1f2932ef8d4fc62b8927d
Aditya-1208 <adityaagr.201ec103@nitk.edu.in>
2022-03-26 18:59:31 +0530

    inti

--- control.v
+++ control.v
@@ -1,4 +1,4 @@
-module control(opcode, RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP);
+module control(opcode, RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch,jump,AluOP);
 
 input [5:0] opcode;
 
@@ -9,18 +9,25 @@ output reg RegWrite;
 output reg MemRead;
 output reg MemWrite;
 output reg Branch;
+output reg NotBranch;
+output reg jump;
 
 output reg [1:0] AluOP;
 
 always @(opcode) begin
 	case (opcode)
-		6'b000000:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'b100100_0_10; //r
-		6'b100011:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'b011110_0_00; //lw
-		6'b101011:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'bx1x001_0_00; //sw
-		6'b000100:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, AluOP}=9'bx0x000_1_01; //beq
+		6'b000000:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch,jump, AluOP}=11'b100100_000_10; //r
+		6'b100011:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch,jump, AluOP}=11'b011110_000_00; //lw
+		6'b101011:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch,jump, AluOP}=11'bx1x001_000_00; //sw
+		6'b000100:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch,jump, AluOP}=11'bx0x000_100_01; //beq
+		6'b001000:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch,jump, AluOP}=11'b010100_000_00; //addi
+		6'b000101:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch,jump, AluOP}=11'bx0x000_010_01; //bne
+		6'b000010:{RegDst, ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch,NotBranch,jump, AluOP}=11'bxxx000_001_01; //j
 		default:
-	{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,AluOP}=9'bxxx_xxx_x_xx;
+		{RegDst,ALUSrc,MemtoReg,RegWrite,MemRead,MemWrite,Branch,NotBranch,AluOP}=11'bxxx_xxx_xx_xx;
 	endcase
 end
 
 endmodule
+
+//refer this for opcode bits "http://alumni.cs.ucr.edu/~vladimir/cs161/mips.html"
